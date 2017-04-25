6fb1ebec7cea59c1e8841fcab825c8181d969704
tatyana222 <Tatyana_Gladysheva@epam.com>
2017-04-26 02:11:41 +0600

    Added basic structure for WebApp, added Gradle support

---
+++ WebApp-Project/src/main/java/com/epam/mentoring/webapp/WebappProjectApplication.java
@@ -0,0 +1,15 @@
+package com.epam.mentoring.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// run app using command:
+// gradlew build && java -jar build/libs/webapp-project-1.0.jar
+// then go to http://localhost:8080
+@SpringBootApplication
+public class WebappProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappProjectApplication.class, args);
+	}
+}
