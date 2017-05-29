1f83d1194a0fda608f003aaa1d3dbb7793101773
tatyana222 <Tatyana_Gladysheva@epam.com>
2017-05-30 02:16:08 +0600

    Fixed basic auth issue

--- WebApp-Project/src/main/java/com/epam/mentoring/webapp/config/SecurityConfig.java
+++ WebApp-Project/src/main/java/com/epam/mentoring/webapp/config/SecurityConfig.java
@@ -3,20 +3,17 @@ package com.epam.mentoring.webapp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
-@EnableWebSecurity
 //@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.httpBasic().and();
+        http.httpBasic().and();
 
         http.antMatcher("/**")
                 .authorizeRequests()
