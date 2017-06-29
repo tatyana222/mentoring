a7c5fee29320344c7a7b629ac97d0bdde5412a8a
tatyana222 <Tatyana_Gladysheva@epam.com>
2017-06-30 02:42:32 +0600

    Added labels and one-element sorting

--- ForkJoin/src/com/epam/mentoring/forkjoin/Main.java
+++ ForkJoin/src/com/epam/mentoring/forkjoin/Main.java
@@ -7,6 +7,7 @@ import java.util.concurrent.ForkJoinPool;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("10 000 elements quicksort");
         Random random = new Random();
         int[] ints = random.ints(10000,0, 10001).toArray();
 
@@ -18,5 +19,15 @@ public class Main {
         pool.invoke(quickSort);
 
         System.out.println(Arrays.toString(ints));
+        System.out.println();
+
+        System.out.println("1 element quicksort");
+        int[] oneElementInts = new int[1];
+        oneElementInts[0] = 1;
+
+        QuickSort oneElementQuickSort = new QuickSort(oneElementInts);
+        pool.invoke(oneElementQuickSort);
+
+        System.out.println(Arrays.toString(oneElementInts));
     }
 }
